c83ec74391da60f4a997401f6eb74c18279c87bb
Fabio Fazio <fabio.fazio@villaggio.org>
2016-11-09 16:57:48 +0100

    Esercizio 1 a partire dal regomento espresso nella nuova cartella di esercizi

--- index.php
+++ index.php
@@ -1,10 +1,43 @@
-<?php
+<?php    
+    class Person {
+        public $name;
+        private $age;
+             
+        public function __construct($name, $age)
+        {
+            $this->name = $name;
+            $this->age = $age;
+        }
+        
+        public static function generatePersonByName($name){
+            $p = new static($name, null);
+            return $p;
+        }
+        
+        public static function generatePersonByAge($age){
+            $p = new static(null, $age);
+            return $p;
+        }
+         
+        private function get_age(){
+            return $this->age;               
+        }
+             
+        public function print_age(){
+            return $this->get_age();
+        }
+        
+        public function toString(){
+            return "Nome: ".$this->name."<br>Eta: ".$this->age."<br>";
+        }
+        }
+ 
+$customer1 = new Person("Giuseppe Rossi","34");
+echo $customer1->toString();
+$customer2 = Person::generatePersonByName("Giuseppe Rossi");
+echo $customer2->toString();
+$customer3 = Person::generatePersonByAge(44);
+echo $customer3->toString();
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
-
+?>
