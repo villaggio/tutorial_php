75d36ec6f3bdaf4adda975075f4f07917d3cd6a8
Fabio Fazio <fabio.fazio@villaggio.org>
2016-11-15 14:40:08 +0100

    Esercizio pomeridiano dedicato a interfaces e abstract classes (testable)

--- esercizi_15/index.php
+++ esercizi_15/index.php
@@ -1,19 +1,7 @@
 <?php    
-require_once "classes/person.php";
 require_once "classes/employees.php";    
  
-$customer1 = new Person("Giuseppe Rossi","34");
+$customer1 = new Employees("Giuseppe Rossi","34", "AD");
 echo $customer1->toString();
 echo "<br>";
-$customer2 = Person::generatePersonByName("Giuseppe Rossi");
-echo $customer2->toString();
-echo "<br>";
-$customer3 = Person::generatePersonByAge(44);
-echo $customer3->toString();
-echo "<br>";
-$customer4 = new Employees("Mario",33,"AD");
-echo $customer4->oldString();
-echo "<br>";
-$customer4->setCompany("Apple");
-echo $customer4->toString();
 ?>
